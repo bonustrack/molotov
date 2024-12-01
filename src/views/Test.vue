012a8da027823339da539defc6e3d86456e2b2c6
less <fabien@bonustrack.co>
2024-12-01 14:59:54 +0700

    fix: display only true boolean keys

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -4,8 +4,17 @@ import { onMounted, ref } from 'vue';
 const keys = ref([]);
 
 function getKeys() {
+  keys.value = [];
+
   if (typeof window !== 'undefined' && window.ethereum) {
-    keys.value = Object.keys(window.ethereum);
+    for (const key in window.ethereum) {
+      if (
+        typeof window.ethereum[key] === 'boolean' &&
+        window.ethereum[key] === true
+      ) {
+        keys.value.push(key);
+      }
+    }
   }
 }
 
